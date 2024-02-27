dde6e9e732561232a34cb0acb0cae47c28c80786
nferreira1 <nathan.1402@hotmail.com>
2024-02-27 13:35:26 -0300

    feat: configuração do Spring finalizado

---
+++ backend/src/test/java/br/edu/senac/ApiProjetoPiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.senac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
