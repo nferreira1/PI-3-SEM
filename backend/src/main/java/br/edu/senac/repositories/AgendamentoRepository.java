2632cf213060426b34066979354234321cd0e6d0
nferreira1 <nathan.1402@hotmail.com>
2024-03-05 08:07:04 -0300

    feat: função de alterar o status criada

--- backend/src/main/java/br/edu/senac/repositories/AgendamentoRepository.java
+++ backend/src/main/java/br/edu/senac/repositories/AgendamentoRepository.java
@@ -1,11 +1,16 @@
 package br.edu.senac.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.edu.senac.models.Agendamento;
 
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+  @Query(nativeQuery = true, value = "UPDATE agendamentos SET status = :status WHERE id = :id")
+  public void alterarStatus(@Param("id") Long id, @Param("status") Agendamento.Status status);
 
 }
