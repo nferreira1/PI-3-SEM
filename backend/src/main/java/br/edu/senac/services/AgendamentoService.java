ccf5c7b0a5cdb458bf5931b2f13d7c583e02e09c
nferreira1 <nathan.1402@hotmail.com>
2024-03-07 08:12:34 -0300

    fix: correção de anotação @Service

--- backend/src/main/java/br/edu/senac/services/AgendamentoService.java
+++ backend/src/main/java/br/edu/senac/services/AgendamentoService.java
@@ -4,12 +4,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.senac.models.Agendamento;
 import br.edu.senac.models.Usuario;
 import br.edu.senac.repositories.AgendamentoRepository;
 
+@Service
 public class AgendamentoService {
 
   @Autowired
