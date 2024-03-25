a93352654ab751b088ea775650092da7cbf670d4
nferreira1 <nathan.1402@hotmail.com>
2024-03-25 07:13:02 -0300

    feat: exclusão de query no repository da entidade AgendamentoStatus

--- backend/src/main/java/br/edu/senac/repositories/AgendamentoStatusRepository.java
+++ backend/src/main/java/br/edu/senac/repositories/AgendamentoStatusRepository.java
@@ -6,8 +6,4 @@ import br.edu.senac.models.AgendamentoStatus;
 
 public interface AgendamentoStatusRepository extends JpaRepository<AgendamentoStatus, Byte> {
 
-  // @Query(nativeQuery = true, value = "SELECT * FROM agendamento_status WHERE id
-  // = :id")
-  // AgendamentoStatus buscarPorId(Byte id);
-
 }
