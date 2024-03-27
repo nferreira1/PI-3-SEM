c65243d0ccf5bc4a35851ffbaf8c7715dc042a48
nferreira1 <nathan.1402@hotmail.com>
2024-03-27 17:18:09 -0300

    feat: classe dark adicionada ao layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} dark`}>{children}</body>
     </html>
   );
 }
