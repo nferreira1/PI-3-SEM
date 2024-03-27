179b37c91d39fb0764e8d6171bf664239ad503f8
nferreira1 <nathan.1402@hotmail.com>
2024-03-27 18:04:23 -0300

    fix: correção de description do metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,7 +6,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SysClub",
-  description: "Sistema de agendamentos de clubes",
+  description:
+    "SysClub é o seu assistente digital definitivo para agendar e organizar atividades esportivas. Com uma interface amigável e recursos robustos, facilitamos a conexão de entusiastas do esporte, permitindo a reserva de espaços, a participação em eventos locais e a organização de jogos com amigos. Seja para encontrar um parceiro de tênis, marcar uma partida de futebol ou explorar novas atividades esportivas na sua área, o SysClub é a plataforma perfeita para manter-se ativo e engajado na comunidade esportiva. Junte-se a nós para simplificar como você planeja, participa e desfruta de esportes!",
   icons: [
     {
       rel: "icon",
