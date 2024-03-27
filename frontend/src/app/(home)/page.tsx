4df5a555221426a5279d4fa7baebd3629b5e072f
nferreira1 <nathan.1402@hotmail.com>
2024-03-27 17:21:05 -0300

    feat: mensagem de bem-vindo adicionada

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,5 +1,25 @@
 import Header from "@/components/header";
 
 export default function Home() {
-  return <Header></Header>;
+  const now = new Date();
+  const month =
+    now.toLocaleDateString("pt-BR", { month: "long" }).charAt(0).toUpperCase() +
+    now.toLocaleDateString("pt-BR", { month: "long" }).slice(1);
+  const weekdayString = now.toLocaleDateString("pt-BR", { weekday: "long" });
+  const weekdayNumber = now.toLocaleDateString("pt-BR", { day: "numeric" });
+  const dayString =
+    weekdayString.charAt(0).toUpperCase() +
+    weekdayString.slice(1).split("-")[0];
+  const fullDay = `${dayString}, ${weekdayNumber} de ${month}`;
+
+  return (
+    <div>
+      <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Nathan!</h2>
+        <p className="text-sm">{fullDay}</p>
+      </div>
+    </div>
+  );
 }
