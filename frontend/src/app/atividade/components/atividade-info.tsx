9614034dc6516d97b373bb8e427510ba824f9b16
nferreira1 <nathan.1402@hotmail.com>
2024-03-28 10:03:57 -0300

    feat: componente atividade-info da página atividade criado

---
+++ frontend/src/app/atividade/components/atividade-info.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface AtividadeInfoProps {
+  atividade: Atividade;
+}
+
+const AtividadeInfo = ({ atividade }: AtividadeInfoProps) => {
+  const router = useRouter();
+
+  const handleVoltarClique = () => router.back();
+
+  return (
+    <div>
+      <div className="h-[250px] w-full relative">
+        <Button
+          onClick={handleVoltarClique}
+          size="icon"
+          variant="outline"
+          className="z-50 absolute top-4 left-4"
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Button
+          size="icon"
+          variant="outline"
+          className="z-50 absolute top-4 right-4"
+        >
+          <MenuIcon />
+        </Button>
+
+        <Image
+          src={atividade?.imagem ?? ""}
+          alt={`imagem de ${atividade.nome}`}
+          className="object-cover"
+          fill
+        />
+      </div>
+
+      <div className="px-5 py-3 border-solid border-secondary">
+        <h1 className="text-xl font-bold">{atividade.nome}</h1>
+        <div className="flex items-center gap-1">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{atividade.local}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AtividadeInfo;
