356b651d10ba51cdda3c3b84211f352341309235
nferreira1 <nathan.1402@hotmail.com>
2024-03-28 14:02:15 -0300

    feat: inserção de função para ir para a página de reserva da atividade

--- frontend/src/components/agendamento-item.tsx
+++ frontend/src/components/agendamento-item.tsx
@@ -1,14 +1,36 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface Props {
   atividade: Atividade;
 }
 
 const AgendamentoItem = ({ atividade }: Props) => {
+  const router = useRouter();
+
+  /**
+   * Função para redirecionar o usuário para a página de reserva da {@link Atividade}.
+   * @see {@link Atividade}
+   * @function
+   * @returns {void}
+   * @example
+   * <Button onClick={handleRouteAtividade}>Detalhes</Button>
+   * @example
+   * <div onClick={handleRouteAtividade}>Detalhes</div>
+   * @example
+   * <Link href="/atividade/[id]" as={`/atividade/${atividade.id}`}>
+   *    <a onClick={handleRouteAtividade}>Detalhes</a>
+   * </Link>
+   */
+  const handleRouteAtividade = (): void =>
+    router.push(`/atividade/${atividade.id}`);
+
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="p-1">
@@ -41,7 +63,11 @@ const AgendamentoItem = ({ atividade }: Props) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
             {atividade?.local}
           </p>
-          <Button variant="secondary" className="w-full mt-3">
+          <Button
+            variant="secondary"
+            className="w-full mt-3"
+            onClick={handleRouteAtividade}
+          >
             Reservar
           </Button>
         </div>
