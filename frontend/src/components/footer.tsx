e88daa17179d695ec5fe8a44604770c67a39dac6
nferreira1 <nathan.1402@hotmail.com>
2024-03-28 09:48:59 -0300

    feat: componente footer criado

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-secondary py-6 px-5">
+      <p className="text-xs text-gray-400 font-semibold opacity-75">
+        © 2024 Copyright SysClub
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
