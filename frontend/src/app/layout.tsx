f581fd759cc9e6820bb7fc5297af1e8030ffeb93
nferreira1 <nathan.1402@hotmail.com>
2024-03-28 09:49:27 -0300

    feat: componente footer implementado no layout do projeto

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.className} dark`}>{children}</body>
+      <body className={`${inter.className} dark`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
