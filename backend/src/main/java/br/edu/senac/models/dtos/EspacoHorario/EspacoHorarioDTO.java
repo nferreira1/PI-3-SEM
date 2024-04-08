03fe3f4acc74d1c1ee39536b2f73ade16fda2916
nferreira1 <nathan.1402@hotmail.com>
2024-04-08 16:07:14 -0300

    feat: formatação de horário

--- backend/src/main/java/br/edu/senac/models/dtos/EspacoHorario/EspacoHorarioDTO.java
+++ backend/src/main/java/br/edu/senac/models/dtos/EspacoHorario/EspacoHorarioDTO.java
@@ -1,10 +1,8 @@
 package br.edu.senac.models.dtos.EspacoHorario;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class EspacoHorarioDTO {
@@ -12,5 +10,11 @@ public class EspacoHorarioDTO {
   private Long id;
   private String horarioInicial;
   private String horarioFinal;
+
+  public EspacoHorarioDTO(Long id, String horarioInicial, String horarioFinal) {
+    this.id = id;
+    this.horarioInicial = horarioInicial.replaceFirst("^(\\d{2}:\\d{2}):\\d{2}$", "$1");
+    this.horarioFinal = horarioFinal.replaceFirst("^(\\d{2}:\\d{2}):\\d{2}$", "$1");
+  }
 
 }
