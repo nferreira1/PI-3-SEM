c6e3a3dc19fd94d8787140c94cd450d4678608fd
nferreira1 <nathan.1402@hotmail.com>
2024-04-09 16:58:36 -0300

    feat: implementação do zod e componente form no componente dialog-login

--- frontend/src/components/dialog-login.tsx
+++ frontend/src/components/dialog-login.tsx
@@ -1,51 +1,54 @@
-"use client";
-
-import { Loader2, LogInIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Loader2 } from "lucide-react";
 import {
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "./ui/dialog";
 import { Button } from "./ui/button";
 import { useSession } from "@/hooks/useSession";
 import { Input } from "./ui/input";
-import { NextResponse } from "next/server";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { useForm, useFormState } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type UsuarioRequest = {
-  email: string;
-  senha: string;
-};
+type UsuarioRequest = z.infer<typeof formSchema>;
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .email({
+      message: "E-mail inválido.",
+    })
+    .min(3)
+    .regex(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/),
+  senha: z.string(),
+});
 
 const DialogLogin = () => {
-  const { login } = useSession();
-  const router = useRouter();
-
-  const [usuario, setUsuario] = useState<UsuarioRequest>({
-    email: "",
-    senha: "",
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      senha: "",
+    },
   });
-  const [loading, setLoading] = useState<boolean>(false);
 
-  const handleLoginClique = async () => {
-    setLoading(true);
+  const { login } = useSession();
+  const { isSubmitting } = useFormState(form);
 
-    try {
-      const response = (await login(
-        "nathan.1402@hotmail.com",
-        "Nathansupergato14"
-      )) as NextResponse;
-
-      console.log(response.status);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
+  const handleSubmit = (data: UsuarioRequest) => {
+    new Promise((resolve) => setTimeout(resolve, 10000));
+    // login(data.email, data.senha);
   };
 
   return (
@@ -55,37 +58,64 @@ const DialogLogin = () => {
           Faça login na plataforma
         </DialogTitle>
         <DialogDescription className="text-center text-gray-400">
-          Conecte-se usando a sua matrícula e os 4 primeiros digitos do CPF.
+          Conecte-se usando o seu e-mail e a sua senha.
         </DialogDescription>
       </DialogHeader>
-
       <DialogFooter className="flex flex-col mt-2 gap-3">
-        <div className="flex gap-3">
-          <Input
-            className="w-[134px]"
-            type="text"
-            placeholder="E-mail"
-            value={usuario?.email}
-            onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}
-          />
-          <Input
-            className="w-[134px]"
-            type="password"
-            maxLength={50}
-            placeholder="Senha"
-            value={usuario?.senha}
-            onChange={(e) => setUsuario({ ...usuario, senha: e.target.value })}
-          />
-        </div>
-
-        <Button
-          className="w-full"
-          onClick={handleLoginClique}
-          disabled={loading}
-        >
-          {loading && <Loader2 className="mr-2 w-4 h-4 animate-spin" />}
-          {loading ? "Carregando..." : "Login"}
-        </Button>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="flex flex-col gap-4"
+          >
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field, fieldState: { error } }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>E-mail</FormLabel>
+                    <FormControl>
+                      <Input
+                        className={`w-[268px] ${
+                          error && "focus-visible:ring-destructive"
+                        }`}
+                        type="text"
+                        placeholder="example@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage>{error?.message}</FormMessage>
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              name="senha"
+              control={form.control}
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Senha</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="w-[268px]"
+                        type="password"
+                        placeholder="********"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                );
+              }}
+            />
+            <Button type="submit" className="w-full" disabled={isSubmitting}>
+              {isSubmitting && (
+                <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+              )}
+              {isSubmitting ? "Carregando..." : "Login"}
+            </Button>
+          </form>
+        </Form>
       </DialogFooter>
     </DialogContent>
   );
