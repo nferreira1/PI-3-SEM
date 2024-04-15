a7079eedbf62f7e8651ee9c6074fc71c0b305291
nferreira1 <nathan.1402@hotmail.com>
2024-04-15 16:08:27 -0300

    feat: remoção de objeto não usado

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,10 +2,8 @@
 const nextConfig = {
     env: {
         API_BASE_URL: "http://localhost:8080",
-        COOKIE_NAME: "SysClubAuthToken",
-        JWT_SECRET: "1bba15653f35eb3a72b8c1f25bbc727a044079f71d38cc67d9c7b2f11e8b198bde1872d12fbac9cecaac103794f13651"
+        COOKIE_NAME: "SysClubAuthToken"
     }
-
 };
 
 export default nextConfig;
