bfb7a7a76b6cbd722d99cef42b8b2a0cbe23670f
nferreira1 <nathan.1402@hotmail.com>
2024-04-23 07:49:39 -0300

    feat: implementação de ordenação por nome

--- backend/src/main/java/br/edu/senac/repositories/AtividadeRepository.java
+++ backend/src/main/java/br/edu/senac/repositories/AtividadeRepository.java
@@ -17,23 +17,24 @@ public interface AtividadeRepository extends JpaRepository<Atividade, String> {
   public List<Atividade> findAllByStatusTrueAndEspacosNotNullAndEspacosStatusTrue();
 
   @Query(nativeQuery = true, value = """
-      SELECT
-      A.*,
-      MAX(E.id) AS espaco_id, 
-      MAX(E.imagem) AS espaco_imagem, 
-      MAX(E.nome) AS espaco_nome, 
-      MAX(E.status) AS espaco_status, 
-      MAX(E.atividade_id) AS espaco_atividade_id,
-      MAX(EH.id) AS espacos_horarios_id, 
-      MAX(EH.espaco_id) AS espacos_horarios_espaco_id, 
-      MAX(EH.horario_id) AS espacos_horarios_horario_id
-      FROM atividades AS A
-      INNER JOIN espacos AS E ON E.atividade_id = A.id AND E.status = 1
-      INNER JOIN espacos_horarios AS EH ON EH.espaco_id = E.id
-      INNER JOIN horarios AS H ON H.id = EH.horario_id AND H.status = 1
-      WHERE A.status = 1
-      GROUP BY A.id;
-  """)
+        SELECT
+        A.*,
+        MAX(E.id) AS espaco_id,
+        MAX(E.imagem) AS espaco_imagem,
+        MAX(E.nome) AS espaco_nome,
+        MAX(E.status) AS espaco_status,
+        MAX(E.atividade_id) AS espaco_atividade_id,
+        MAX(EH.id) AS espacos_horarios_id,
+        MAX(EH.espaco_id) AS espacos_horarios_espaco_id,
+        MAX(EH.horario_id) AS espacos_horarios_horario_id
+        FROM atividades AS A
+        INNER JOIN espacos AS E ON E.atividade_id = A.id AND E.status = 1
+        INNER JOIN espacos_horarios AS EH ON EH.espaco_id = E.id
+        INNER JOIN horarios AS H ON H.id = EH.horario_id AND H.status = 1
+        WHERE A.status = 1
+        GROUP BY A.id
+        ORDER BY A.nome;
+      """)
   public List<Atividade> buscarTodasAtividades();
 
 }
