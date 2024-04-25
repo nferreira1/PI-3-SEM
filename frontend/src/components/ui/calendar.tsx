ab4c23e0e2a320a649836ee7d8f86f8b80e07934
nferreira1 <nathan.1402@hotmail.com>
2024-04-25 16:50:07 -0300

    fix: correção de estilos do componente calendar

--- frontend/src/components/ui/calendar.tsx
+++ frontend/src/components/ui/calendar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import * as React from "react";
 import { DayPicker } from "react-day-picker";
 
-import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { ptBR } from "date-fns/locale";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
@@ -16,18 +16,9 @@ function Calendar({
   showOutsideDays = true,
   ...props
 }: CalendarProps) {
-  const hoje = new Date();
-  const semana = new Date(hoje);
-  semana.setDate(hoje.getDate() + 6);
-
-  const disabledDays = [{ before: hoje }, { after: semana }];
-
   return (
     <DayPicker
       locale={ptBR}
-      fromDate={hoje}
-      toDate={semana}
-      disabled={disabledDays}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
@@ -55,7 +46,7 @@ function Calendar({
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
-          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
+          "aria-selected:bg-primary aria-selected:text-primary-foreground",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
