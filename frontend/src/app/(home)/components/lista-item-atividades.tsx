a5bb5b05294b6794d08a3593bef347c4949dfd24
nferreira1 <nathan.1402@hotmail.com>
2024-04-26 19:57:11 -0300

    feat: componente lista-item-atividades criado

---
+++ frontend/src/app/(home)/components/lista-item-atividades.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { useSession } from "@/hooks/useSession";
+import ItemAtividade from "./item-atividade";
+
+const ListaItemAtividades = ({
+  atividades,
+}: {
+  atividades: Atividade[] | null;
+}) => {
+  const { status } = useSession();
+
+  const className =
+    "px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden";
+
+  if (status === "loading") {
+    return (
+      <>
+        <Skeleton className="h-4 w-32 mb-3.5" />
+
+        <div className={className}>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Skeleton key={index} className="w-[167px] rounded-2xl" />
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h2 className="px-5 text-xs text-muted-foreground font-bold mb-3 uppercase">
+        Recomendados
+      </h2>
+
+      <div className={className}>
+        {atividades?.map((atividade) => (
+          <ItemAtividade key={atividade.id} atividade={atividade} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ListaItemAtividades;
