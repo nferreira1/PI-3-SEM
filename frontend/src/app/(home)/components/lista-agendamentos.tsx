baff1f59a03ed6ee27f53d798cc9535325811698
nferreira1 <nathan.1402@hotmail.com>
2024-04-26 19:47:12 -0300

    feat: componente lista-agendamentos criado

---
+++ frontend/src/app/(home)/components/lista-agendamentos.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import ItemReserva from "@/components/item-reserva";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useSession } from "@/hooks/useSession";
+
+const ListaAgendamentos = ({
+  agendamentos,
+}: {
+  agendamentos: Agendamento[] | undefined;
+}) => {
+  const { status } = useSession();
+
+  const className =
+    "px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden";
+
+  if (status === "loading") {
+    return (
+      <>
+        <Skeleton className="h-4 w-32 mb-3.5" />
+
+        <div className={className}>
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Skeleton key={index} className="min-w-full h-[126px]" />
+          ))}
+        </div>
+      </>
+    );
+  }
+
+  return (
+    status === "authenticated" && (
+      <>
+        <h2 className="text-xs text-muted-foreground font-bold mb-3 uppercase">
+          Agendamentos
+        </h2>
+
+        <div className={className}>
+          {agendamentos?.map((agendamento) => (
+            <ItemReserva key={agendamento.id} agendamento={agendamento} />
+          ))}
+        </div>
+      </>
+    )
+  );
+};
+
+export default ListaAgendamentos;
