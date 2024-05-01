34fb0b656964384cd2c433a3d2dcfd8f66f29fba
nferreira1 <nathan.1402@hotmail.com>
2024-05-01 20:34:34 -0300

    feat: mudança de texto ao clicar em copiar no componente telefone

--- frontend/src/components/item-agendamento/telefone.tsx
+++ frontend/src/components/item-agendamento/telefone.tsx
@@ -1,16 +1,30 @@
 "use client";
 
 import { Smartphone } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 
 const Telefone = ({ telefone }: { telefone: string }) => {
+  const [clique, setClique] = useState<boolean>(false);
+
   const handleCopiarTexto = async (texto: string) => {
     try {
       await navigator.clipboard.writeText(texto);
+      setClique(true);
     } catch (error) {
       console.error("Erro ao copiar texto", error);
     }
   };
+
+  useEffect(() => {
+    if (clique) {
+      const timer = setTimeout(() => {
+        setClique(false);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [clique]);
 
   return (
     <div className="w-full flex justify-between items-center mb-2">
@@ -18,8 +32,14 @@ const Telefone = ({ telefone }: { telefone: string }) => {
         <Smartphone />
         <p>{telefone}</p>
       </div>
-      <Button variant="outline" onClick={() => handleCopiarTexto(telefone)}>
-        Copiar
+      <Button
+        variant="outline"
+        onClick={() => handleCopiarTexto(telefone)}
+        className={`transition-colors w-24 ${
+          clique ? "bg-green-700 hover:bg-green-700" : "bg-background"
+        }`}
+      >
+        {clique ? "Copiado" : "Copiar"}
       </Button>
     </div>
   );
