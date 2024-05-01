ce3ece3eb3b7511f6953e0cc74c8083daa86946d
nferreira1 <nathan.1402@hotmail.com>
2024-05-01 16:50:36 -0300

    feat: adição de nova configuração na entidade configuracoes

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -46,6 +46,14 @@ INSERT IGNORE INTO configuracoes(id, nome, valor, descricao, status) VALUES (
 
 INSERT IGNORE INTO configuracoes(id, nome, valor, descricao, status) VALUES (
   3,
+  "TEMPO_VERIFICACAO_AGENDAMENTO_AGUARDANDO_CONFIRMACAO",
+  "1",
+  "Tempo em que o sistema verifica os agendamentos que estão aguardando confirmação e se já passaram da data atual (o valor deve ser em minutos).",
+  1
+);
+
+INSERT IGNORE INTO configuracoes(id, nome, valor, descricao, status) VALUES (
+  4,
   "TEMPO_ANTES_DATA_PARA_CONFIRMAR_AGENDAMENTO",
   "1",
   "Tempo anterior a data, em que o usuário tem para confirmar o agendamento (o valor deve ser em minutos). Exemplo: Se o valor for 60, o usuário tem 60 minutos antes da data do agendamento para confirmar.",
