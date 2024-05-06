236e05dfe3f5ae387443e7b6e2c3cafa5a4fe634
nferreira1 <nathan.1402@hotmail.com>
2024-05-06 18:04:41 -0300

    fix: correção de componente header e side-menu

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,14 +1,11 @@
-import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import SideMenu from "./side-menu";
 
 const Header = () => {
   return (
-    <Card className="rounded-none">
+    <Card className="rounded-none lg:px-32">
       <CardContent className="p-5 flex flex-row justify-between">
         <Link href="/">
           <Image
@@ -18,17 +15,7 @@ const Header = () => {
             width={200}
           />
         </Link>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <MenuIcon size={20} />
-            </Button>
-          </SheetTrigger>
-
-          <SheetContent className="p-0">
-            <SideMenu />
-          </SheetContent>
-        </Sheet>
+        <SideMenu />
       </CardContent>
     </Card>
   );
--- frontend/src/components/header/side-menu.tsx
+++ frontend/src/components/header/side-menu.tsx
@@ -6,6 +6,7 @@ import {
   HomeIcon,
   LogInIcon,
   LogOutIcon,
+  MenuIcon,
   UserRound,
 } from "lucide-react";
 import Link from "next/link";
@@ -13,87 +14,105 @@ import { AlertDialog, AlertDialogTrigger } from "../ui/alert-dialog";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Dialog, DialogTrigger } from "../ui/dialog";
-import { SheetHeader, SheetTitle } from "../ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
 import AlertLogout from "./alert-logout";
 import DialogContent from "./dialog";
 
 const SideMenu = () => {
-  const { data, status, login } = useSession();
+  const { data, status } = useSession();
 
   return (
-    <>
-      <SheetHeader>
-        <SheetTitle>Menu</SheetTitle>
-      </SheetHeader>
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon" className="flex lg:hidden">
+          <MenuIcon size={20} />
+        </Button>
+      </SheetTrigger>
 
-      {status === "authenticated" ? (
-        <div className="flex justify-between px-5 py-6 items-center">
-          <div className="flex items-center gap-3">
-            <Avatar>
-              <AvatarImage src={data!.imagem ?? ""} />
-              <AvatarFallback>{data!.nome?.charAt(0)}</AvatarFallback>
-            </Avatar>
+      <SheetContent className="p-0">
+        <SheetHeader>
+          <SheetTitle>Menu</SheetTitle>
+        </SheetHeader>
 
-            <h2 className="font-bold">{data!.nome}</h2>
-          </div>
+        {status === "authenticated" ? (
+          <div className="flex justify-between px-5 py-6 items-center">
+            <div className="flex items-center gap-3">
+              <Avatar>
+                <AvatarImage src={data!.imagem ?? ""} />
+                <AvatarFallback>{data!.nome?.charAt(0)}</AvatarFallback>
+              </Avatar>
 
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button variant="secondary" size="icon">
-                <LogOutIcon size={20} />
-              </Button>
-            </AlertDialogTrigger>
-            <AlertLogout />
-          </AlertDialog>
-        </div>
-      ) : (
-        <div className="flex flex-col px-5 py-6 gap-3">
-          <div className="flex items-center gap-2">
-            <div className="w-10 h-10 rounded-full border-[3px] border-solid text-secondary">
-              <UserRound size={34} />
+              <h2 className="font-bold">{data!.nome}</h2>
             </div>
-            <h2 className="font-bold">Olá, faça seu login!</h2>
-          </div>
-        </div>
-      )}
 
-      <div className="flex flex-col gap-3 px-5">
-        {status === "unauthenticated" && (
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button
-                variant="secondary"
-                className="w-full justify-start rounded-lg"
-              >
-                <LogInIcon className="mr-2" size={18} />
-                Fazer Login
-              </Button>
-            </DialogTrigger>
-            <DialogContent />
-          </Dialog>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Button variant="secondary" size="icon">
+                  <LogOutIcon size={20} />
+                </Button>
+              </AlertDialogTrigger>
+              <AlertLogout />
+            </AlertDialog>
+          </div>
+        ) : (
+          <div className="flex flex-col px-5 py-6 gap-3">
+            <div className="flex items-center gap-2">
+              <div className="w-10 h-10 rounded-full border-[3px] border-solid text-secondary">
+                <UserRound size={34} />
+              </div>
+              <h2 className="font-bold">Olá, faça seu login!</h2>
+            </div>
+          </div>
         )}
 
-        <Button variant="outline" className="justify-start rounded-lg" asChild>
-          <Link href="/">
-            <HomeIcon size={18} className="mr-2" />
-            Início
-          </Link>
-        </Button>
+        <div className="flex flex-col gap-3 px-5">
+          {status === "unauthenticated" && (
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  variant="secondary"
+                  className="w-full justify-start rounded-lg"
+                >
+                  <LogInIcon className="mr-2" size={18} />
+                  Fazer Login
+                </Button>
+              </DialogTrigger>
+              <DialogContent />
+            </Dialog>
+          )}
 
-        {status === "authenticated" && (
           <Button
             variant="outline"
             className="justify-start rounded-lg"
             asChild
           >
-            <Link href="/agendamentos">
-              <CalendarIcon size={18} className="mr-2" />
-              Agendamentos
+            <Link href="/">
+              <HomeIcon size={18} className="mr-2" />
+              Início
             </Link>
           </Button>
-        )}
-      </div>
-    </>
+
+          {status === "authenticated" && (
+            <Button
+              variant="outline"
+              className="justify-start rounded-lg"
+              asChild
+            >
+              <Link href="/agendamentos">
+                <CalendarIcon size={18} className="mr-2" />
+                Agendamentos
+              </Link>
+            </Button>
+          )}
+        </div>
+      </SheetContent>
+    </Sheet>
   );
 };
 
