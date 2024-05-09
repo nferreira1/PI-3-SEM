a17bdc544a2a6f89e25bb373d9ec6c05cb9d5550
nferreira1 <nathan.1402@hotmail.com>
2024-05-09 18:56:54 -0300

    feat: criação de layout para a página de agendamentos

---
+++ frontend/src/app/agendamentos/layout.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/header/header";
+
+export default function AgendamentoLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+}
