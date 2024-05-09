9ebe5ebad86d0ee88b2cf110ab617aa306ca2945
nferreira1 <nathan.1402@hotmail.com>
2024-05-09 10:27:10 -0300

    feat: componente card-agendamentos-info criado

---
+++ frontend/src/components/item-agendamento/card-agendamentos-info.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useState } from "react";
+import CardAgendamento from "./card-agendamento";
+import CardContentAgendamento from "./card-content-agendamento";
+
+interface Props {
+  agendamentos: Agendamento[];
+}
+
+const CardAgendamentosInfo = ({ agendamentos }: Props) => {
+  const [agendamentoSelecionado, setAgendamentoSelecionado] =
+    useState<Agendamento | null>();
+
+  const agendamentosAgrupados = agendamentos?.reduce(
+    (acc: { [key: number]: Agendamento[] }, agendamento) => {
+      const statusId = agendamento.status.id - 1;
+      acc[statusId] = acc[statusId] || [];
+      acc[statusId].push(agendamento);
+      return acc;
+    },
+    {}
+  );
+
+  const statusAgendamento = [
+    "Aguardando Confirmação",
+    "Confirmados",
+    "Cancelados",
+    "Finalizados",
+  ];
+
+  return (
+    <div className="w-full flex space-x-4 justify-center">
+      <div className="w-1/2 space-y-4">
+        {statusAgendamento.map((status, index) => {
+          const agendamentosPorStatus =
+            (agendamentosAgrupados && agendamentosAgrupados[index]) ?? [];
+
+          return (
+            <div key={`${status}-${index}`}>
+              <h2 className="text-sm font-bold uppercase text-gray-400 mt-6 mb-3">
+                {status}
+              </h2>
+
+              <div className="space-y-4">
+                {agendamentosPorStatus?.map((agendamento) => (
+                  <div
+                    key={agendamento.id}
+                    onClick={() => setAgendamentoSelecionado(agendamento)}
+                    className="cursor-pointer max-w-[524px]"
+                  >
+                    <CardAgendamento agendamento={agendamento} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="w-1/2 pt-6">
+        {agendamentoSelecionado && (
+          <motion.div className="sticky top-6">
+            <CardContentAgendamento
+              setAgendamentoSelecionado={setAgendamentoSelecionado}
+              agendamento={agendamentoSelecionado}
+            />
+          </motion.div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CardAgendamentosInfo;
