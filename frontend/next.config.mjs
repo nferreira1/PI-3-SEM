30abd99624c43e1800b9f981f6cecb59a7016835
nferreira1 <nathan.1402@hotmail.com>
2024-05-20 08:00:02 -0300

    feat: adição de remote pattern para uso de imagens do pinterest

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        API_BASE_URL: "http://localhost:8080",
-        COOKIE_NAME: "SysClubAuthToken"
-    }
+  env: {
+    API_BASE_URL: "http://localhost:8080",
+    COOKIE_NAME: "SysClubAuthToken"
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "i.pinimg.com",
+        pathname: "**",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
