20a9768d0dd8031ab9963192ecd0f64d3b0b0cc7
nferreira1 <nathan.1402@hotmail.com>
2024-05-22 16:37:39 -0300

    feat: modificação de deploy de frontend

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps: [{
     name: "Sysclub",
     script: "npm",
-    args: "start",
+    args: "dev",
     env: {
       NODE_ENV: 'production',
+      HOST: '0.0.0.0',
       PORT: 80
     }
   }]
